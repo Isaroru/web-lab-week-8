a872d192659bec7c925ecfaa8fb79e2fc37229e7
Isaroru <isabella.rodriguez2@u.icesi.edu.co>
2023-04-02 22:32:10 -0500

    nike cards

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,20 +1,46 @@
-class card extends HTMLElement {
+class Card extends HTMLElement {
     constructor() {
         super()
         this.attachShadow({
             mode: 'open'
         })
+        this.name = this.getAttribute('name')
+        this.url = this.getAttribute('url')
+        this.description = this.getAttribute('description')
+        this.category = this.getAttribute('category')
+        this.collection = this.getAttribute('collection')
+        this.colors = this.getAttribute('color')
+        this.price = this.getAttribute('price')
+    }
 
-    }
     static get observedAttributes() {
-        return []
+        return ['name', 'url', 'description', 'category', 'collection', 'colors', 'price']
     }
+
     render() {
         this.shadowRoot.innerHTML = `
-        <link rel = "stylesheet" href = "/src/components/card/style.css"
+        <link rel = "stylesheet" href = "/src/components/card/style.css">
+        <article class="product">
+            <span class="product_price">$${this.price}</span>
+            <img class="product_image" src="${this.url}">
+            <h1 class="product_title">${this.name}</h1>
+            <hr />
+            <h3>${this.category}</h3>
+            <p>${this.description}</p>
+            <a href="#" class="product_btn btn">SHOP</a>
+        </article>
         `
     }
     connectedCallback() {
         this.render()
     }
+    
+    attributeChangedCallback(propName, oldValue, newValue) {
+        console.log(`attr ${propName} changed`)
+        this[propName] = newValue
+        this.render
+    }
 }
+
+customElements.define('nike-card', Card)
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
 import "./components/card/card.js"
+async function getData(){
+    const message = {
+        method: 'GET',
+        headers: {
+            'content-type':'application/json'
+        }
+    }
+    const API = await fetch('https://64207a7425cb6572104a3f53.mockapi.io/catalog',message)
+    const data = await API.json()
+    console.log(data)
+    return data
+}
+
+const shoesList = await getData();
+const cardsContainer = document.getElementById('cards_container');
+
+console.log(Array.isArray(shoesList))
+
+function renderShoes(){
+    shoesList.forEach(shoe =>{
+        const nikeShoe = document.createElement('nike-card')
+        nikeShoe.setAttribute('name', shoe.name)
+        nikeShoe.setAttribute('price', shoe.price)
+        nikeShoe.setAttribute('url', shoe.url)
+        nikeShoe.setAttribute('category', shoe.category)
+        nikeShoe.setAttribute('description', shoe.description)
+        nikeShoe.setAttribute('colors', shoe.color)
+        nikeShoe.setAttribute('description', shoe.description)
+    
+        cardsContainer.append(nikeShoe)
+    });
+}
+
+renderShoes();
+
 
